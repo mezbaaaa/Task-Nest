2d4040b79842e888b60f7d819c76a06ca15a71b5
mezbaaaa <tahsinul101@gmail.com>
2025-05-22 15:53:15 +0600

    navbar added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../components/Header/Navbar';
 
 const Root = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto'>
+            <header>
+                <Navbar></Navbar>
+            </header>
+            <main>
+                <Outlet></Outlet>
+            </main>
+            <footer></footer>
         </div>
     );
 };
